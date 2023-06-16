84cba4f2f71a6a342e7f0e6b535f7162201aaad3
Butawantemi <japhetbuta@gmail.com>
2023-06-16 03:15:23 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific text with proper grammar
+ *              and a new line using printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     printf("with proper grammar, but the outcome is a piece of art,\n");
     return 0;
 }
-
