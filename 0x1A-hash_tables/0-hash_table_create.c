084fbbb0f19cb22835b54b8a07e1f7903f148e68
Butawantemi <japhetbuta@gmail.com>
2023-10-06 17:43:24 +0300

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,15 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new_table = NULL;
-if (size == 0)
-return (NULL);
-new_table = malloc(sizeof(hash_table_t));
-if (!new_table)
-return (NULL);
-new_table->size = size;
-new_table->array = calloc((size_t)size, sizeof(hash_node_t *));
-if (new_table->array == NULL)
-return (NULL);
-return (new_table);
+	hash_table_t *new_ht = NULL;
+
+	if (size == 0)
+		return (NULL);
+	new_ht = malloc(sizeof(hash_table_t));
+	if (!new_ht)
+		return (NULL);
+
+	new_ht->size = size;
+	new_ht->array = calloc((size_t)size, sizeof(hash_node_t *));
+
+	if (new_ht->array == NULL)
+		return (NULL);
+
+	return (new_ht);
 }
