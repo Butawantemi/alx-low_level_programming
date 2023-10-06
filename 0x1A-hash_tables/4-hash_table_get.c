17a04dd66226c5c391385141213acea13b295b66
Butawantemi <japhetbuta@gmail.com>
2023-10-06 17:15:43 +0300

    0x1A-hash_tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,29 +2,25 @@
 #include <string.h>
 
 /**
- * hash_table_get - Retrieves a value associated with a key.
- * @ht: The hash table to look into.
- * @key: The key you are looking for.
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: the hash table to look into.
+ * @key: the key you are looking for.
  *
  * Return: The value associated with the element, or NULL if key couldn't be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *current;
-
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
-
-    index = key_index((const unsigned char *)key, ht->size);
-    current = ht->array[index];
-
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-            return (current->value);
-        current = current->next;
-    }
-
-    return (NULL);
+unsigned long int index;
+hash_node_t *current;
+if (ht == NULL || key == NULL || *key == '\0')
+return (NULL);
+index = key_index((const unsigned char *)key, ht->size);
+current = ht->array[index];
+while (current != NULL)
+{
+if (strcmp(current->key, key) == 0)
+return (current->value);
+current = current->next;
+}
+return (NULL);
 }
